b3e23cd12ad5670be1410a5fac2c73094af67eab
RanEdelmanGit <ranedelmann@gmail.com>
2024-06-14 14:23:59 +0300

    init commit
    includes installations of router-dom and tailwindCSS.

---
+++ talk-support/src/App.jsx
@@ -0,0 +1,13 @@
+import './styles/App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-red-500 text-2xl font-bold'>Talk Support</h1>
+    </>
+  )
+}
+
+export default App
